4b61bd11ace6739e90b0dc589ba8f8870aceee2d
EiadSS <eiads.sulaiman@gmail.com>
2024-04-28 16:01:32 -0400

    added user entity

---
+++ backend/backend/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using backend.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = new List<User>()
+            {
+                new User
+                {
+                    Id = 1,
+                    firstName = "eiad",
+                    lastName = "suliman",
+                    userName = "eiadss",
+                    email = "eiad@gmail.com",
+                    password = "eiad12345",
+                }
+        };
+            return Ok(users);
+        }
+    }
+}
---
+++ backend/backend/Entity/User.cs
@@ -0,0 +1,15 @@
+﻿namespace backend.Entity
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; } = string.Empty;
+        public string lastName { get; set; } = string.Empty;
+        
+        public string userName { get; set; } = string.Empty;
+
+        public string email { get; set; } = string.Empty;
+
+        public string password { get; set; } = string.Empty;
+    }
+}
